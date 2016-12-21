c11662cce1ea4acafdbeb2123b4e33bfcac521ed
BurningMarshmallow <stanislav-fedjanin@rambler.ru>
2016-12-21 22:56:37 +0500

    Corrected NHunspell result usage

--- TagsCloudApp/TagsCloudVisualization/Statistics/LowerStemWordProcessor.cs
+++ TagsCloudApp/TagsCloudVisualization/Statistics/LowerStemWordProcessor.cs
@@ -10,20 +10,18 @@ namespace TagsCloudVisualization.Statistics
 
         public LowerStemWordProcessor()
         {
-            stemCreator = new Hunspell("NHunspellDictionaries\\en_US.aff",
-                "NHunspellDictionaries\\en_US.dic");
+            var result = Result.Of(() => new Hunspell("NHunspellDicionaries\\en_US.aff",
+                    "NHunspellDictionaries\\en_US.dic"));
+            if (result.IsSuccess)
+                stemCreator = result.Value;
         }
 
         public string ProcessWord(string word)
         {
-            var res = Result.Of(() =>
-            {
-                    var stems = stemCreator.Stem(word);
-                    if (stems.Count > 0)
-                        word = stems.First();
-                return word.ToLower();
-            });
-            return res.IsSuccess ? res.Value : "Error happened";
+            var stems = stemCreator?.Stem(word);
+            if (stems?.Count > 0)
+                word = stems.First();
+            return word.ToLower();
         }
     }
 }
