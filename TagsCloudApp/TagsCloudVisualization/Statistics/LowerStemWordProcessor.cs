470abc724a97fd23fa811dafa062a6ca6b0d1588
BurningMarshmallow <stanislav-fedjanin@rambler.ru>
2016-12-25 00:34:59 +0500

    Small fix in stemProcessor

--- TagsCloudApp/TagsCloudVisualization/Statistics/LowerStemWordProcessor.cs
+++ TagsCloudApp/TagsCloudVisualization/Statistics/LowerStemWordProcessor.cs
@@ -10,7 +10,7 @@ namespace TagsCloudVisualization.Statistics
 
         public LowerStemWordProcessor()
         {
-            var result = Result.Of(() => new Hunspell("NHunspellDictonaries\\en_US.aff",
+            var result = Result.Of(() => new Hunspell("NHunspellDictionaries\\en_US.aff",
                     "NHunspellDictionaries\\en_US.dic"));
             if (result.IsSuccess)
                 stemCreator = result.Value;
@@ -22,8 +22,8 @@ namespace TagsCloudVisualization.Statistics
 
         public string ProcessWord(string word)
         {
-            var stems = stemCreator?.Stem(word);
-            if (stems?.Count > 0)
+            var stems = stemCreator.Stem(word);
+            if (stems.Count > 0)
                 word = stems.First();
             return word.ToLower();
         }
